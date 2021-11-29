82119356a7f236dae72e694e469df7662b078d5b
davidwcheng <68088328+davidwcheng@users.noreply.github.com>
2021-11-29 16:08:16 -0500

    alot of stuff for funsy

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -1,9 +1,22 @@
+import React, { useState, useContext } from 'react'
 import './App.css';
-import { useState } from 'react';
 
+
+
+const styles = {
+  darkBlue: {
+    backgroundColor: '#365391',
+    fontSize: '30px',
+  },
+  holyWhite: {
+    backgroundColor: '#FFFFFF',
+    fontSize: '30px',
+  }
+}
+
+const themeCtx = React.createContext(styles.darkBlue);
 
 function App() {
-
   const images = {
     "cat": "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d",
     "dog": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
@@ -12,17 +25,35 @@ function App() {
     "dawg": "https://external-preview.redd.it/fX-BW8AT4MiiVpu9TzKk9EsOYNqpIrljDD5gao07V4k.jpg?auto=webp&s=f0e8bfa3676f55c6e5657c412ded98dafbad4f5f",
     "beans": "https://i.pinimg.com/474x/92/62/3b/92623baf166d9faf2e77fecd355a0545.jpg",
     "aang": "https://preview.redd.it/8kpwuq79h4851.jpg?auto=webp&s=fd50d6f2d0d4da2e55f636733c73153ff9789ae6",
-    "spange": "https://api.time.com/wp-content/uploads/2019/08/caveman-spongebob-spongegar.png" 
+    "spange": "https://api.time.com/wp-content/uploads/2019/08/caveman-spongebob-spongegar.png"
   }
+
   const [image, setImage] = useState();
   return (
     <div className="App">
-      <h1>Type into the textbox below if you want to, idk idc</h1>
+      <h1>Type into the textbox below if you want to BING BONG, idk idc</h1>
+      <themeCtx.Provider value={styles.darkBlue}>
+        <h2>themed text box bing bong</h2>
+        <TextBox />
+      </themeCtx.Provider>
       <header className="App-header">
         <input type="text" style={{ width: "200px", height: "50px", fontFamily: "Comic Sans MS", fontSize: "30px", marginBottom: "10px" }} onChange={(event) => setImage(event.target.value)} />
         <img style={{ backgroundSize: "contain", height: "400px", width: "auto" }} src={images[image]} />
       </header>
+  
     </div>
+  );
+}
+function TextBox(props) {
+  return (
+      <ThemedText />
+  );
+}
+
+function ThemedText() {
+  const theme = useContext(themeCtx);
+  return ( 
+    <input type="text" style={{ backgroundColor: theme.backgroundColor, fontSize: theme.fontSize, height: "auto", width: "auto" }}/>
   );
 }
 
